cab0e8de55f5cd41d523a5d929c2d6055bdd9c01
JobinK-Thomas <jobinthomas@ikm.gov.in>
2026-02-24 15:23:36 +0530

    UI update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
       <h1 className="text-4xl font-bold mb-4">
-        🚀 CI/CD Practice App is working fine
+        🚀 CI/CD Practice App v2
       </h1>
 
       <p className="text-xl mb-2">
