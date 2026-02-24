462dc96348745b7afa6522e70a35f32eaf70cdbf
JobinK-Thomas <jobinthomas@ikm.gov.in>
2026-02-24 16:02:59 +0530

    content Changed

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
       <h1 className="text-4xl font-bold mb-4">
-        Hi Boss
+        Hai malini 
       </h1>
 
       <p className="text-xl mb-2">
-        Enthanu Parupadi
+        intha naa unkitta sollia aavanum 
       </p>
 
       <p className="text-lg mb-2">
@@ -15,7 +15,7 @@ export default function Home() {
       </p>
 
       <p className="text-green-400 mt-4">
-        Chaya kudikkan Poyo
+        nee avlo azhagu
       </p>
     </main>
   );
