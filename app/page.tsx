d66b43305938bf7a2feb4852721005b88c61a928
JobinK-Thomas <jobinthomas@ikm.gov.in>
2026-02-24 15:13:26 +0530

    checking deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
-  const version = "1.0.1";
+  const version = "1.0.2";
   const time = new Date().toLocaleString();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
       <h1 className="text-4xl font-bold mb-4">
-        🚀 CI/CD Practice App
+        🚀 CI/CD Practice App is working fine
       </h1>
 
       <p className="text-xl mb-2">
