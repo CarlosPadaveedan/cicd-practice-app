c65c080bce920754a4bc1253aa62ee5de0c9d873
JobinK-Thomas <jobinthomas@ikm.gov.in>
2026-02-24 15:43:44 +0530

    content Changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,21 @@
 export default function Home() {
-  const version = "1.0.2";
-  const time = new Date().toLocaleString();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
       <h1 className="text-4xl font-bold mb-4">
-        🚀 CI/CD Practice App v2
+        Hi Boss
       </h1>
 
       <p className="text-xl mb-2">
-        Version: {version}
+        Enthanu Parupadi
       </p>
 
       <p className="text-lg mb-2">
-        Deployed at: {time}
+        {/* Deployed at: {time} */}
       </p>
 
       <p className="text-green-400 mt-4">
-        If you see this updated after push → CI/CD working!
+        Chaya kudikkan Poyo
       </p>
     </main>
   );
